f1e9d2af23e199ccba6ca6b416c42c83368df643
Andi Usman Balo <andibalo213@gmail.com>
2024-08-28 15:43:06 +0700

    week 1 homework

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Andi Usman Balo - 00000037809</Text>
       <StatusBar style="auto" />
     </View>
   );
